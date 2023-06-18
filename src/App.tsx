938258740f3b74a940f7a6c15ce54dd7a6ec0fa2
Edgar Jacquemoud <59689456+Nyuu-s@users.noreply.github.com>
2023-06-18 21:37:50 +0200

    fixed 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Animes from './Pages/Animes'
+import Custom404 from './Pages/404'
 function App() {
   return (
     <> 
       <Routes> 
         <Route path='/' element={<Home />}/>
         <Route path='/list' element={<Animes />}/>
+        <Route path="*" element={<Custom404 />} />
       </Routes>
     </>
   )
