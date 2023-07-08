53f88ec010d1773ed2b9f3e4234f1da16e1c8631
Edgar Jacquemoud <59689456+Nyuu-s@users.noreply.github.com>
2023-07-08 16:34:56 +0200

    postcss edit

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {config: './tailwind.config.js'},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+
   },
 }
