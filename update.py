c1c4ac685285995da88499872ebec4226a27f6f4
Edgar Jacquemoud <59689456+Nyuu-s@users.noreply.github.com>
2023-08-26 15:32:03 +0200

    Update update.py

--- update.py
+++ update.py
@@ -2,11 +2,11 @@ import requests
 import json
 import os
 import subprocess
-import sys
+
 
 gist_id = os.environ['GIST_ID']
 token = os.environ['GITHUB_TOKEN']
-app_name = os.environ['APP_NAME']
+filename = 'update.json'
 
 
 headers = {
@@ -14,21 +14,14 @@ headers = {
     'Accept': 'application/vnd.github.v3+json',
 }
 
-notes = sys.argv[1]
-subprocess.check_call(['git', 'fetch', '--tags'])
-tags = subprocess.check_output(['git', 'tag']).decode('utf-8').strip()
-print('Tags:', tags)
+version = subprocess.check_output(['git', 'fetch', '-all', '-tags']).decode('utf-8').strip()
 version = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode('utf-8').strip()
-
 # Function to generate the signature for a platform
-def generate_signature(platform):
-    # Execute the Tauri Sign CLI command for the platform
-    signature = subprocess.check_output(['tauri', 'sign', platform]).decode('utf-8').strip()
-    return signature
+
 
 def generate_urls(target):
     # Execute the Tauri Sign CLI command for the platform
-    url = f"https://github.com/Nyuu-s/AnimeListv10/releases/download/v{version}/{app_name}_{version}_{target}"
+    url = f"https://github.com/Nyuu-s/AnimeListv10/releases/download/v{version}/AnimesList10_{version}_{target}"
     return url
 
 data = {
@@ -36,22 +29,21 @@ data = {
         'update.json': {
             'content': json.dumps({
                 'version': version,
-                'notes': notes,
                 'platforms': {
                     'darwin-x86_64': {
-                        'signature': generate_signature('darwin-x86_64'),
+                        'signature': "",
                         'url': generate_urls('x86_64.app.tar.gz')
                     },
                     'darwin-aarch64': {
-                        'signature': generate_signature('darwin-aarch64'),
+                        'signature': "",
                         'url': generate_urls('aarch64.app.tar.gz')
                     },
                     'linux-x86_64': {
-                        'signature': generate_signature('linux-x86_64'),
+                        'signature': "",
                         'url': generate_urls('amd64.AppImage.tar.gz')
                     },
                     'windows-x86_64': {
-                        'signature': generate_signature('windows-x86_64'),
+                        'signature': "",
                         'url': generate_urls('x64-setup.nsis.zip')
         
                     }
@@ -60,6 +52,10 @@ data = {
         }
     }
 }
+
+response = requests.patch(f'https://api.github.com/gists/{gist_id}', headers=headers, data=json.dumps(data))
+
+response.raise_for_status()
 
 response = requests.patch(f'https://api.github.com/gists/{gist_id}', headers=headers, data=json.dumps(data))
 
