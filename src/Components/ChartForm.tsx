cc9e411d797f752519fc40b0b71101a71db85c66
Edgar Jacquemoud <59689456+Nyuu-s@users.noreply.github.com>
2023-10-19 23:39:12 +0200

    chart column

--- src/Components/ChartForm.tsx
+++ src/Components/ChartForm.tsx
@@ -1,7 +1,9 @@
 import { Button, Center, Select, TextInput, TransferListItem } from "@mantine/core"
 import { useDataState } from "../context"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { ChartData, ChartType, DatasetChart, SimpleTableData } from "./Helpers/useCustomTypes"
+import { RecordNoID, T_RecordNoID } from "./Helpers/useRecord";
+import { title } from "process";
 
 const clamp = (num:number, min:number, max: number) => Math.min(Math.max(num, min), max);
 function GetChartDataForCartesianProd(Table: SimpleTableData, ID: number, visibility: boolean, title: string, chartType: ChartType, tooltip: string): ChartData {
@@ -76,7 +78,7 @@ function ChartForm() {
     "Pie": ChartType.CPie,
     "Lines": ChartType.CLine
   }
-    const {SimpleStatTablesData, chartsCollection, addChart} = useDataState()
+    const {SimpleStatTablesData, chartsCollection, addChart, getHeaders, getPossibleValues, getData} = useDataState()
     const [ChartTitle, setChartTitle] = useState<string>()
     const [SelectChartType, setSelectChartType] = useState<string>(Object.keys(ChartTypeMap)[0])
     const [SelectOverlayLabel, setSelectOverlayLabel] = useState<string>()
@@ -85,13 +87,48 @@ function ChartForm() {
     const [isSColumnDisabled, setisSColumnDisabled] = useState<boolean>(false)
     const [isSTableDisabled, setisSTableDisabled] = useState<boolean>(false)
 
+    const HeaderList = useMemo(() => getHeaders().map((v) => ({value: v.header, label: v.header})).sort((a, b) =>  (a.label > b.label)  ? 1 : -1), [])
 
 
     useEffect(() => {
-      setisSColumnDisabled(SelectColumn !== undefined)
-      setisSTableDisabled(SelectTable !== undefined)
+      console.log(SelectColumn);
+      
+      setisSColumnDisabled(SelectColumn !== undefined && SelectColumn !== null )
+      setisSTableDisabled(SelectTable !== undefined && SelectTable !== null )
     }, [SelectTable, SelectColumn])
     
+
+  function getChartDataFromColumn(str: string) {
+      const pValues = getPossibleValues(str)
+      const data = Object.values(getData())
+
+      
+      const Restricted = data.reduce((acc: any, curr) => {
+        const currNoID = curr as T_RecordNoID
+        const currStr = currNoID[str]
+
+        
+        if(pValues.includes(`${currStr.value}`))
+        {
+          if(currStr.value === "")
+          {
+            const value = "Empty Value"
+            acc[value] = acc[value] === undefined ? 1 : acc[value] += 1
+          }
+          else
+          {
+            acc[currStr.value] = acc[currStr.value] === undefined ? 1 : acc[currStr.value] += 1
+          }
+        }
+      return acc
+      }, {})
+
+      
+
+      
+
+      return Restricted
+  }
 
   return (
     <div className="mt-5">
@@ -106,48 +143,58 @@ function ChartForm() {
         <Select
         disabled={isSColumnDisabled }
         value={SelectTable}
+        clearable
         onChange={(e) => setSelectTable(e as string)}
         label={ <div className={`${ isSColumnDisabled || SimpleStatTablesData.length > 0 ?  '' :'text-gray-700' }`}>Table </div>  }
         data={[...SimpleStatTablesData.map((v) => ({label: `${v.id} - ${v.title} `, value: `${v.id}`}) )]} />
 
         <Select
         value={SelectColumn}
+        clearable
         onChange={(e) => setSelectColumn(e as string)}
         disabled={isSTableDisabled}
         label={<div className={`${isSTableDisabled ? 'text-gray-700': ''}`}>Column </div>}
-        data={["Column1", "Column2"]} />
+        data={HeaderList.filter((v) => v.label !== 'ID')} />
          <Center className='mt-5'>
           
             <Button className="mr-5" variant="filled" color="green" onClick={() => {
+              const ID =  chartsCollection.length > 0 ? chartsCollection.sort((a, b) => a.id > b.id ? 1 : -1)[chartsCollection.length-1].id+1 : 0
 
-               const ID =  chartsCollection.length > 0 ? chartsCollection.sort((a, b) => a.id > b.id ? 1 : -1)[chartsCollection.length-1].id+1 : 0
-               const datasetsArray: DatasetChart = {
-                label: SelectOverlayLabel as string,
-                data: SimpleStatTablesData[parseInt(SelectTable as string)].dataCounts.slice(0, SimpleStatTablesData[parseInt(SelectTable as string)].maxSize.MaxRows).map(arr => arr.slice(0, SimpleStatTablesData[parseInt(SelectTable as string)].maxSize.MaxCols)).flat(),
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.4)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1
-               }
-               const cartesianProd: ChartData = GetChartDataForCartesianProd(SimpleStatTablesData[parseInt(SelectTable as string)], ID, true, ChartTitle as string, ChartTypeMap[SelectChartType], SelectOverlayLabel as string )
-               const rowlabels = GetLabelsForRow(SimpleStatTablesData, parseInt(SelectTable as string))
-               const collabels = GetLabelsForCol(SimpleStatTablesData, parseInt(SelectTable as string))
-               const addingChart: ChartData = cartesianProd
-              
-              addChart(addingChart)
+              //Table Mode
+              if(!isSColumnDisabled && isSTableDisabled)
+              {
+
+                
+                const cartesianProd: ChartData = GetChartDataForCartesianProd(SimpleStatTablesData[parseInt(SelectTable as string)], ID, true, ChartTitle as string, ChartTypeMap[SelectChartType], SelectOverlayLabel as string )
+                const rowlabels = GetLabelsForRow(SimpleStatTablesData, parseInt(SelectTable as string))
+                const collabels = GetLabelsForCol(SimpleStatTablesData, parseInt(SelectTable as string))
+                const addingChart: ChartData = cartesianProd
+               
+               addChart(addingChart)
+              }
+              else // Column Mode
+              {
+                  const chartData = getChartDataFromColumn(SelectColumn as string)
+                  const labels = Object.keys(chartData)
+                  const counts: number[] = Object.values(chartData)
+                  const backgrounds :string[] = []
+                  labels.forEach(_ => {
+                    const r = clamp(Math.round(Math.random() * 255), 15, 255)
+                    const g = clamp(Math.round(Math.random() * 255), 25, 255)
+                    const b = clamp(Math.round(Math.random() * 255), 30, 255)
+                    backgrounds.push(`rgba(${r}, ${g}, ${b}, 0.5)`)
+                  })
+                  const dataset: DatasetChart = 
+                    {
+                      data: counts,
+                      backgroundColor: backgrounds,
+                      borderColor: [],
+                      borderWidth: 1,
+                      label: "#"
+                    }
+                  
+                  addChart({labels, id: ID, title: ChartTitle as string, visibility: true, type: ChartTypeMap[SelectChartType], datasets: [dataset]})
+              }
             }}>Add Chart</Button>
             <Button variant="filled" color="yellow">Cancel</Button>
          </Center>
